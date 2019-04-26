c00ed7ca211c8d41a8b7ecd0d298010dc9fd7327
Akgun Demirbas <ademirbas@shutterfly.com>
2019-04-26 12:52:52 +0300

    Change api

--- exchange.py
+++ exchange.py
@@ -6,23 +6,24 @@ import requests
 
 def fetch_exchanges(codes, target):
     currencies = []
-    URL = 'https://free.currencyconverterapi.com/api/v5/convert?q={query}&compact=y'
-    target = target.upper()
-    for code in codes:
-        code = code.upper()
-        query = '{code}_{target}'.format(code=code, target=target)
-        response = requests.get(URL.format(query=query)).json()
-        if query in response:
-            currencies.append({
-                'code': code,
-                'rate': str(response[query]['val'])
-            })
+    URL = 'https://api.exchangeratesapi.io/latest?base={base}&symbols={symbols}'
+    base = target.upper()
+    symbols = ','.join(code.upper() for code in codes)
+    response = requests.get(URL.format(base=base, symbols=symbols)).json()
+    if not 'rates' in response:
+        sys.exit(2)
+    rates = response['rates']
+    for symbol in rates.keys():
+        currencies.append({
+            'code': symbol,
+            'rate': 1/rates[symbol]
+        })
     return currencies
 
 
 @click.command()
 @click.option('--delimiter', '-d', default=':')
-@click.option('--target', '-t', default='TRY')
+@click.option('--target', '-t', default='TRY', help='Currency symbol you want to get rates for.')
 @click.argument('code', nargs=-1)
 def exchange(delimiter, target, code):
     """Show exchange rates"""
@@ -30,7 +31,8 @@ def exchange(delimiter, target, code):
         click.echo('Provide currency code.')
         sys.exit(1)
     exchanges = fetch_exchanges(code, target)
-    lines = [exchange['code'] + delimiter + exchange['rate'] for exchange in exchanges]
+    lines = [exchange['code'] + delimiter + '{:.4f}'.format(exchange['rate'])
+             for exchange in exchanges]
     click.echo(', '.join(lines))
 
 
